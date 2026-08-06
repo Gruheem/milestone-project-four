52a80c261469f2b90e9a3fc217fec5c63476bf8c
Gruheem <grahamsilcock91@gmail.com>
2026-08-06 13:25:15 +0100

    Add Checkout Form, Checkout Signals and urls.py

---
+++ checkout/signals.py
@@ -0,0 +1,23 @@
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderLineItem)
+# Takes the sender of the signal: OrderLineItem, the instance of the model that was saved, 
+# a boolean indicating whether the instance was created or updated, and any additional keyword arguments.
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
+
