74c9478da5a615418a545f5a452eb3d651c8d190
Gruheem <grahamsilcock91@gmail.com>
2026-08-18 10:45:36 +0100

    Make Search Bar Collapse

--- static/js/base.js
+++ static/js/base.js
@@ -4,4 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
             autohide: false
         }).show();
     });
+
+    const searchBtn = document.getElementById('search-btn')
+    const searchInput = document.getElementById('search-input')
+    searchBtn.addEventListener('click', function() {
+        searchInput.classList.remove('d-none');
+        searchInput.focus();
+        // Change the button type to 'submit'. A delay of 0 just brings it to the back of the event queue, 
+        // allowing the click event to finish before changing the type.
+        setTimeout(() => {
+            searchBtn.type = 'submit';
+        }, 0);
+    });
 });
